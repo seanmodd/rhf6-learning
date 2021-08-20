0624d9dbe36ea177a79a19c755915989580c8b83
Sean <sean@vestaridefair.com>
2021-08-20 13:37:14 -0700

    ok

---
+++ src/App.js
@@ -0,0 +1,75 @@
+/* eslint-disable jsx-a11y/label-has-associated-control, react/button-has-type */
+
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import './App.css';
+
+function App() {
+  const { register, handleSubmit, errors } = useForm();
+  const [userInfo, setUserInfo] = useState();
+  const onSubmit = (data) => {
+    setUserInfo(data);
+    console.log(data);
+  };
+  console.log(errors);
+  return (
+    <div className="container">
+      <pre>{JSON.stringify(userInfo, undefined, 2)}</pre>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <h1>Registration Form</h1>
+        <div className="ui divider" />
+        <div className="ui form">
+          <div className="field">
+            <label>Username</label>
+            <input
+              type="text"
+              name="username"
+              placeholder="Username"
+              ref={register({ required: 'Username is required' })}
+            />
+          </div>
+          <p>{errors.username?.message}</p>
+          <div className="field">
+            <label>Email</label>
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              ref={register({
+                required: 'Email is required',
+                pattern: {
+                  value: /^\S+@\S+$/i,
+                  message: 'This is not a valid email',
+                },
+              })}
+            />
+          </div>
+          <p>{errors.email?.message}</p>
+          <div className="field">
+            <label>Password</label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              ref={register({
+                required: 'Password is required',
+                minLength: {
+                  value: 4,
+                  message: 'Password must be more than 4 characters',
+                },
+                maxLength: {
+                  value: 10,
+                  message: 'Password cannot exceed more than 10 characters',
+                },
+              })}
+            />
+          </div>
+          <p>{errors.password?.message}</p>
+          <button className="fluid ui button blue">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default App;
